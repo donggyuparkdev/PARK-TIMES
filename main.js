48f2a8636e4e3181cf7f414fb37487239aac4f2e
DongGyu Park <donggyu.park.dev@gmail.com>
2022-03-26 00:20:18 +0900

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const getLatestNews = () => {
-    let url = `https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=sport&page_size=10`
+    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=sport&page_size=10`);
     console.log(url)
+    let header = new Headers({'x-api-key':'YKJZl_zdGEuG32fp8PS8abauyu6RdGRgqpz0XqeqNUI'});
+    let response = fetch(url,{headers:header});
 }
 
-getLatestNews()
+getLatestNews();
