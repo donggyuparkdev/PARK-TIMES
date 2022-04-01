19bd36e93f658063e8953e8c42461191adba0ed0
DongGyu Park <donggyu.park.dev@gmail.com>
2022-04-01 23:43:05 +0900

    first commit

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ let news = [];
 let menus = document.querySelectorAll(".menus button")
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByTopic(event)));
 
-const getLatestNews = async () => {
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=sport&page_size=10`);
-    console.log(url)
+let searchButton = document.getElementById("search-button");
+let url;
+// 각 함수에 필요한 url 만든다
+// api 호출 함수를 부른다
+
+const getNews = async () => {
     let header = new Headers({
         'x-api-key': 'YKJZl_zdGEuG32fp8PS8abauyu6RdGRgqpz0XqeqNUI'
     });
@@ -20,23 +23,33 @@ const getLatestNews = async () => {
     render();
 };
 
+const getLatestNews = async () => {
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=sport&page_size=10`);
+    // console.log(url)    
+    getNews();
+};
+
 const getNewsByTopic = async (event) => {
     console.log("클릭됨", event.target.textContent);
     let topic = event.target.textContent.toLowerCase();
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&page_size=10&topic=${topic}`);
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&page_size=10&topic=${topic}`);
     // console.log("uuu",url)
+    // console.log("토픽뉴스", data);
+    getNews();
+};
 
-    let header = new Headers({
-        'x-api-key': 'YKJZl_zdGEuG32fp8PS8abauyu6RdGRgqpz0XqeqNUI'
-    });
-    let response = await fetch(url, {
-        headers: header
-    });
-    let data = await response.json();
-    news = data.articles;
-    render();
+const getNewsByKeyword = async () => {
+    // 검색 키워드 읽어오기
+    // url에 검색 키워드 붙이기
+    // 헤더준비
+    // url부르기
+    // 데이터 가져오기
+    // 데이터 보여주기
 
-    console.log("토픽뉴스", data);
+    let keyword = document.getElementById("search-input").value;
+    console.log(keyword);
+    url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`);
+    getNews();
 };
 
 const render = () => {
@@ -62,4 +75,5 @@ const render = () => {
     document.getElementById("news-board").innerHTML = newsHTML;
 }
 
+searchButton.addEventListener("click", getNewsByKeyword);
 getLatestNews();
